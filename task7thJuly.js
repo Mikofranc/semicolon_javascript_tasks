e26f418fefb895f435c907b8998a09625b55f999
mikofranc <m.ogbechie@native.semicolon.africa>
2023-07-07 10:24:21 +0100

    task updated

---
+++ task7thJuly.js
@@ -0,0 +1,16 @@
+// let prompt = require('prompt-sync')();
+var s = "codeleet";
+var arr = [4, 5, 6, 7, 0, 2, 1, 3];
+var newWord = "";
+var z =""
+
+for(let index = 0; index < arr.length; index++){
+     let check = arr[index];
+    for(let j =0; j < arr.length; j++){
+        newWord = s[check];
+    }
+    z += newWord
+}
+console.log(z);
+
+
